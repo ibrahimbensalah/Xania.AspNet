8a743019dd0110c3cc28535b1581ed8c4960b23a
ibrahimbensalah <ibrahim.bensalah@gmail.com>
2014-11-23 18:12:19 +0100

    Simulator unit tests

---
+++ Xania.AspNet.Simulator.Tests/MvcRequestTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+
+namespace Xania.AspNet.Simulator.Tests
+{
+    public class MvcRequestTests
+    {
+
+        [Test]
+        public void ActionExecuteTest()
+        {
+            // arange
+            var controller = new TestController();
+
+            // act
+            var result = controller.Execute(c => c.Index());
+
+            // assert
+            Assert.AreEqual("Hello Simulator!", result.ViewBag.Title);
+        }
+
+        [Test]
+        public void UnAuthorizedActionTest()
+        {
+            // arrange 
+            var controller = new TestController();
+
+            // act 
+            var result = controller.Execute(c => c.UserProfile());
+
+            // assert
+            Assert.IsInstanceOf<HttpUnauthorizedResult>(result.ActionResult);
+        }
+
+
+        [Test]
+        public void AuthorizedActionTest()
+        {
+            // arrange 
+            var controller = new TestController();
+
+            // act 
+            var result = controller.Action(c => c.UserProfile()).Authenticate("user", null).Execute();
+
+            // assert
+            Assert.IsInstanceOf<ViewResult>(result.ActionResult);
+        }
+    }
+
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Hello Simulator!";
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
