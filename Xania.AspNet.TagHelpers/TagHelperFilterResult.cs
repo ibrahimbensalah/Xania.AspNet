e9ee995528185a5c99f7499213b37e9d47d1dbc7
Ibrahim ben Salah <ibrahim.bensalah@gmail.com>
2015-02-25 21:54:28 +0100

    added taghelper filter attribute and actionResult

---
+++ Xania.AspNet.TagHelpers/TagHelperFilterResult.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace Xania.AspNet.TagHelpers
+{
+    public class TagHelperFilterResult : ActionResult
+    {
+        private readonly ActionResult _actionResult;
+        private readonly TagHelperProvider _tagHelperProvider;
+
+        public TagHelperFilterResult(ActionResult actionResult, TagHelperProvider tagHelperProvider)
+        {
+            _actionResult = actionResult;
+            _tagHelperProvider = tagHelperProvider;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            if (response.Filter != null)
+            {
+                response.Filter = new HtmlProcessor(new StreamWriter(response.Filter, response.ContentEncoding),
+                    _tagHelperProvider);
+            }
+            else
+            {
+                response.Output = new TagHelperWriter(response.Output, _tagHelperProvider);
+            }
+            _actionResult.ExecuteResult(context);
+        }
+    }
+}
---
+++ Xania.AspNet.TagHelpers/TagHelperWriter.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Xania.AspNet.TagHelpers
+{
+    internal class TagHelperWriter : TextWriter
+    {
+        private readonly TextWriter _output;
+        private readonly HtmlProcessor _processor;
+
+        public TagHelperWriter(TextWriter output, TagHelperProvider tagHelperProvider)
+            : base(output.FormatProvider)
+        {
+            _output = output;
+            _processor = new HtmlProcessor(output, tagHelperProvider);
+        }
+
+        public override Encoding Encoding
+        {
+            get { return _output.Encoding; }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _output.Dispose();
+        }
+
+        public override void Close()
+        {
+            _output.Close();
+        }
+
+        public override void Flush()
+        {
+            _output.Flush();
+        }
+
+        public override void Write(char value)
+        {
+            var bytes = Encoding.GetBytes(new[] { value });
+            _processor.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
