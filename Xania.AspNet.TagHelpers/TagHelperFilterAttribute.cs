aa2da4237eeb258d9609d6b34cca983c736fd736
ibrahimbensalah <ibrahim.bensalah@gmail.com>
2015-03-01 18:27:59 +0100

    replace GetCustomTagHelpers method with CustomTagHelpers property

--- Xania.AspNet.TagHelpers/TagHelperFilterAttribute.cs
+++ Xania.AspNet.TagHelpers/TagHelperFilterAttribute.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -9,15 +11,17 @@ namespace Xania.AspNet.TagHelpers
     public class TagHelperFilterAttribute : ActionFilterAttribute
     {
         private readonly IDependencyResolver _defaultDependencyResolver;
+        public ICollection<Type> CustomTagHelpers { get; private set; }
 
         public TagHelperFilterAttribute()
             : this(DependencyResolver.Current)
         {
         }
 
-        private TagHelperFilterAttribute(IDependencyResolver defaultDependencyResolver)
+        public TagHelperFilterAttribute(IDependencyResolver defaultDependencyResolver)
         {
             _defaultDependencyResolver = defaultDependencyResolver;
+            CustomTagHelpers = new Collection<Type>();
         }
 
 
@@ -28,12 +32,39 @@ namespace Xania.AspNet.TagHelpers
 
         protected virtual ActionResult GetFilterResult(ActionExecutedContext filterContext)
         {
-            return new TagHelperFilterResult(_defaultDependencyResolver, filterContext.Result, GetCustomTagHelpers());
+            var q = from th in CustomTagHelpers
+                from name in GetTagNames(th)
+                select new KeyValuePair<string, Type>(name, th);
+
+            return new TagHelperFilterResult(_defaultDependencyResolver, filterContext.Result, q);
         }
 
-        protected virtual IEnumerable<KeyValuePair<string, Type>> GetCustomTagHelpers()
+        private IEnumerable<string> GetTagNames(Type tagHelperType)
         {
-            return Enumerable.Empty<KeyValuePair<string, Type>>();
+            var tagNameAttr = tagHelperType.GetCustomAttribute<TagNameAttribute>();
+            if (tagNameAttr != null)
+            {
+                foreach (var name in tagNameAttr.Names)
+                    yield return name;
+            }
+            else
+            {
+                var typeName = tagHelperType.Name;
+                if (typeName.EndsWith("TagHelper"))
+                    yield return typeName.Substring(0, typeName.Length - "TagHelper".Length);
+                else
+                    yield return typeName;
+            }
         }
+    }
+
+    public class TagNameAttribute: Attribute
+    {
+        public TagNameAttribute(params String[] names)
+        {
+            Names = names;
+        }
+
+        public virtual string[] Names { get; private set; }
     }
 }
