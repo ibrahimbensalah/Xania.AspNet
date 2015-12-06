945a7e65c51d57e20d7f35739a49bc5aa4f35b23
Ibrahim ben Salah <ibrahim.bensalah@gmail.com>
2015-12-06 22:25:45 +0100

    completed support for net40 and net45

--- Xania.AspNet.Simulator/nupack.cmd
+++ Xania.AspNet.Simulator/nupack.cmd
@@ -1,6 +1,6 @@
 msbuild
 
-msbuild ..\Xania.AspNet.Http\Xania.AspNet.Http.csproj /p:TargetFrameworkVersion=v4.5
+msbuild Xania.AspNet.Simulator.csproj /p:Configuration=NET45 /p:TargetFrameworkVersion=v4.5 /p:DefineConstants="NET45;TRACE" /p:OutputPath=bin\DebugNet45\
 
 .\.nuget\nuget.exe pack -IncludeReferencedProjects .\Xania.AspNet.Simulator.csproj
 pause
